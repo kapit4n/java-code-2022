5acd80a059a0e0e13128994f27e4431fd79857af
Luis Arce <luis.arce22@gmail.com>
2022-11-11 17:13:17 -0400

    Init java code counter

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Categories
+## OOP
+instances-11-11-2022.java(104), S(104)
+
+## PIVOT
+S(0)
+
+# Total 
+104
---
+++ count.py
@@ -0,0 +1,43 @@
+import os 
+import pathlib
+
+fReadme = open("./Readme.md", "w")
+
+## Linear count
+def countLines(path):
+    count = 0
+    for f in os.listdir(path):
+        fPath = path + "/" + f
+        file_ext = pathlib.Path(fPath).suffix
+        if (os.path.isfile(fPath) and file_ext in ['.java']):
+            fCount = len(open(fPath).readlines())
+            fReadme.write(str(f) + "(" + str(fCount) + "), ")
+            count = count + fCount
+    fReadme.write("S(" + str(count) + ")\n")
+    return count
+
+next_goal = 100
+
+total = 0
+fReadme.write("## Categories")
+for f in os.listdir('./src-ig'):
+    pathFull = "./src-ig/" + f
+    fReadme.write("\n## " + str(f).upper() + "\n")
+    if (os.path.isdir(pathFull)):
+        total = total + countLines(pathFull)
+
+for f in os.listdir('./src'):
+    pathFull = "./src/" + f
+    fReadme.write("\n## " + str(f).upper() + "\n")
+    if (os.path.isdir(pathFull)):
+        total = total + countLines(pathFull)
+
+if (total >= next_goal):
+    print(total, "GOAL REACHED")
+else:
+    print("Total: ", total, " and ", (next_goal - total), "FOR THE GOAL")
+
+
+
+
+fReadme.write("\n# Total \n" + str(total))
